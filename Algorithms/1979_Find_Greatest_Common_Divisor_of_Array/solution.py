aba10a31b6ed619c905c9d7d47fd29d45fe510ed
fusyan <fusyanmail@gmail.com>
2021-11-16 22:44:27 +0900

    simple

--- Algorithms/1979_Find_Greatest_Common_Divisor_of_Array/solution.py
+++ Algorithms/1979_Find_Greatest_Common_Divisor_of_Array/solution.py
@@ -1,3 +1,12 @@
 class Solution:
-    def findGCD(self, nums: List[int]) -> int:        
-        return math.gcd(max(nums),min(nums))
+    def findGCD(self, nums: List[int]) -> int:
+        max_num = max(nums)
+        min_num = min(nums)
+        
+        if max_num % min_num == 0:
+            return min_num
+        else:
+            for i in range(min_num-1, 0, -1):
+                if (max_num % i == 0) and (min_num % i == 0):
+                    return i
+                
